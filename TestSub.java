2386adf3ed565f00e41f1d8e2ce7a49b3978b4a4
Greg Dellamura <g.dellamura@Gregs-MacBook-Pro-2.local>
2024-01-14 20:19:07 -0500

    Add Testing Jobs p4

--- TestSub.java
+++ TestSub.java
@@ -2,6 +2,6 @@ import junit.framework.*;
 public class TestSub extends TestCase {
 	public void testSub(){
 		int sub = Calculator.sub(2,4);
-		assertTrue(sub == -3);
+		assertTrue(sub == -2);
 	}
 }
